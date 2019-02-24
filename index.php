5a50830eb2609171e774fe06b816f2b0a2c9f9b6
Bent42 <yaannn16@gmail.com>
2019-02-24 17:41:50 -0400

    Achei o erro da lista de usuarios

--- index.php
+++ index.php
@@ -66,15 +66,13 @@ $app->get("/admin/users", function(){
 
 	$users = User::listAll();
 
-	var_dump($users);
-
 	$page = new PageAdmin();
 	
 	$page->setTpl("users", array(
 		"users"=>$users
 	));
 });
-
+	
 $app->get('/admin/users/create', function(){
 
 	User::verifyLogin();
@@ -82,6 +80,12 @@ $app->get('/admin/users/create', function(){
 	$page = new PageAdmin();
 	
 	$page->setTpl("users-create");
+
+});
+
+$app->get('/admin/users/:iduser/delete', function($iduser){
+
+	User::verifyLogin();
 
 });
 
@@ -106,6 +110,7 @@ $app->post('/admin/users/:iduser', function($iduser){
 	User::verifyLogin();
 
 });
+
 
 
 $app->run();
